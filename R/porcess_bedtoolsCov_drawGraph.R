286fdfc0b95a342bb54b30bbc072ef4f3eded4ab
m-zaki <zaki.fadlullah@icloud.com>
2016-04-06 15:38:18 +0100

    Added example data

--- R/porcess_bedtoolsCov_drawGraph.R
+++ R/porcess_bedtoolsCov_drawGraph.R
@@ -1,64 +1,9 @@
 # Stage 3 
 # Drawing the actual graph 
 
-workDir <- "/Volumes/StemCellBiol1/Personal Folders/zaki/project/misc/20160329_transcriptGraph"
-
+library(binr)
 library(dplyr)
 library(ggplot2)
-
-# Now use the bed file to ; 
-# 1) Generate exonCoverage percentage
-# 2) Generae 5' to 3' graph
-
-# Now use bedtools coverage to calculate individual base coverage
-# module load apps/bedtools/2.25.0/gcc-4.4.7 
-
-# Convert bam to bed
-## bedtools coverage -a toy.bed -b sample.bed -d  -s 
-
-#  bedtools coverage -a /scratch/zaki/file/ref/zaki_customBED/EnsDb.Mmusculus.v75_proteinCoding_example.bed -b WT_r1.bed -d  -s > eg.txt
-
-# -a = the bed originated from  the BAM
-# -b = The EnsDb reference 
-# -d = report per base coverage
-# -s = only report things with the same strand 
-# -S = report opposite strandess (for duTP protocol)
-
-# Two genes coverage
-dat <- read.delim(file.path(workDir, "bedtoolsCov_output/eg.txt"), header=F)
-
-# Divide data into list based on individual gene
-dat_l <- split( dat , f = ee$V5 ) #column V5 is the ensbl gene_id
-
-# For one gene
-gene_strand <- dat_l[[1]]$V6 # column V6 has the strand infomation
-gene_name <- dat_l[[1]]$V5 # column V9 has the gene name
-cov_value <- dat_l[[1]]$V9 # column V9 contains the coverage
-# Eg - want to bin the data into 100 bins
-binfunc <- bins(1:nrow(dat_l[[1]]), target.bins = 100, max.breaks = 100)
-# Extract result from binfunction
-binlength <- as.numeric(binfunc$binct) # This contains the number of continous data that makes one bin
-# Create a category of the bin
-binlist <- list()
-for (i in seq_along(binlength)){
-  binlist[[i]] <-  rep(i, each=binlength[i])
-}
-bincateg <- unlist(binlist) 
-# Data frame containing the coverage and the bin it belongs to
-df_cov <- data.frame(value = cov_value,
-                     bin = bincateg,
-                     gene_name = gene_name,
-                     strand = gene_strand)
-# Get the mean of each bin
-df_bin <- as.data.frame(group_by(df_cov, bin) %>% 
-                          summarise(mean_cov = mean(value),
-                                    gene_name = unique(gene_name),
-                                    strand = unique(strand)))
-
-plot(x=df_bin$bin, y=rev(df_bin$mean))
-
-ggplot(df_bin, aes(x=bin, y=rev(mean_cov))) +
-  geom_line() 
 
 # ------------------------------------- #
 # Processing in loops
@@ -66,21 +11,20 @@ ggplot(df_bin, aes(x=bin, y=rev(mean_cov))) +
 
 workDir <- "/Volumes/StemCellBiol1/Personal Folders/zaki/project/misc/20160329_transcriptGraph"
 
-# Two genes coverage
-dat <- read.delim(file.path(workDir, "bedtoolsCov_output/eg20.txt"), header=F)
+# Read the an example data which contains example ouput from bedtools coverage (across 20 genes)
+dat <- read.delim(file.path(workDir, "data/bedtoolsCov_output/eg20.txt"), header=F)
 
+# Divide data into list based on individual gene 
+# This step takes the longest on a big data (anyway of doing this with dplyr??)
+dat_l <- split( dat , f = dat$V4 ) #column V4 is the ensembl gene_id
 
-# Divide data into list based on individual gene
-dat_l <- split( dat , f = dat$V4 ) #column V4 is the ensmbl gene_id
-
-# Splid data with dplyr
-dat_plyr <- group_by(dat, V4)
-
+# Create empty list for loop
 cov_list <- list()
 perc_cov <- vector()
 
-# Make everything into a loop
-for (i in 1:length(group_size(dat_plyr))){
+# Make everything into a loop 
+# Refer previous github commit for the script version which does not deal with loops
+for (i in seq_along(dat_l)){
   gene_strand <- as.character(dat_l[[i]]$V6) # column V6 has the strand infomation
   gene_name <- as.character(dat_l[[i]]$V5) # column V5 has the name
   gene_id <- as.character(dat_l[[i]]$V4) # column V4 has the gene_id
@@ -117,16 +61,21 @@ for (i in 1:length(group_size(dat_plyr))){
 # Unlist the cov_list into a data frame
 bin_cov <- do.call("rbind", cov_list)
 
-# For one gene BEGIN
-pos_gene <- filter(bin_cov, gene_name == "Gm12169") %>% group_by(bin) %>% 
+# ----------- For one gene BEGIN
+# If we are interested to plot just for one gene
+pos_gene <- filter(bin_cov, gene_name == "Eif4g3") %>% group_by(bin) %>% 
   summarise(numOfgene = length(bin),
             totalValue = sum(mean_cov))
 
 ggplot(pos_gene, aes(x=bin, y=totalValue)) +
   geom_line() 
-# For one gene END
+# ----------- For one gene END
 
-# Now for all gene
+# Now plotting for all gene
+# Need to seperate genes according to strand because ;
+# for positive gene the transcirpt orientation is : 5' ----------- 3' 
+# for negative gene the transcirpt orientation is : 3' ----------- 5' 
+# Therefore need to reverese the direction of negative gene
 pos_gene <- filter(bin_cov, strand == "+") %>% group_by(bin) %>% 
   summarise(numOfgene = length(bin),
             totalValue = sum(mean_cov))
@@ -135,18 +84,38 @@ neg_gene <- filter(bin_cov, strand == "-") %>% group_by(bin) %>%
   summarise(numOfgene = length(bin),
             totalValue = sum(mean_cov))
 
+# Combine the coverage from positive gene and negative gene according to thier bins
 totalCov <- pos_gene$totalValue + rev(neg_gene$totalValue)
-
-totalGene <- pos_gene$numOfgene + rev(neg_gene$numOfgene)
+totalGene <- pos_gene$numOfgene + rev(neg_gene$numOfgene) # Total number of genes (needs the number to calculate mean)
 
 meanCov <- totalCov / totalGene
-
-plot(x=1:100, y=meanCov)
 
 df_bin <- data.frame(bin = 1:100,
                      mean_cov <- meanCov)
 
 ggplot(df_bin, aes(x=bin, y=rev(mean_cov))) +
-  geom_line() 
+  geom_line() +
+  ylab("Read count") +
+  xlab("5' to 3' coverage")
 
 
+# ----------------------------- #
+# Check strand bias
+# ----------------------------- #
+
+# Combine the coverage from positive gene and negative gene according to thier bins
+totalCov_strand <- c(pos_gene$totalValue , rev(neg_gene$totalValue))
+totalGene_strand <- c(pos_gene$numOfgene , rev(neg_gene$numOfgene)) # Total number of genes (needs the number to calculate mean)
+
+meanCov_strand <- totalCov_strand / totalGene_strand
+
+df_bin_strand <- data.frame(bin = rep(1:100,2),
+                            mean_cov = meanCov_strand,
+                            strand = c(rep("pos", 100),
+                                       rep("neg", 100)))
+
+
+ggplot(df_bin_strand, aes(x=bin, y=rev(mean_cov), colour = strand)) +
+  geom_line() +
+  ylab("Read count") +
+  xlab("5' to 3' coverage")
